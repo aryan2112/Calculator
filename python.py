793d1f60f8f4fc09f71e04b4562e2161aa9e6aaa
Aryan Rai <78547046+aryan2112@users.noreply.github.com>
2022-01-14 21:01:09 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,16 @@
+print("Calculator")
+print("Type 'quit' to exit")
+run= True
+equation=0
+
+def calculate():
+    global run
+    global previous
+    equation = input("Enter equation: ")
+    if equation=='quit':
+        run=False
+    else:
+        previous=eval(equation)
+        print(previous)
+while run:
+    calculate()
