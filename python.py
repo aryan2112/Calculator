dd69763292a95793474127b996ced220cc3edf61
Aryan Rai <78547046+aryan2112@users.noreply.github.com>
2022-01-22 14:06:53 +0530

    Update python.py

--- python.py
+++ python.py
@@ -6,7 +6,7 @@ equation=0
 def calculate():
     global run
     global previous
-    equation = input("Enter equation - ")
+    equation = input("Enter equation : ")
     if equation=='quit':
         run=False
     else:
